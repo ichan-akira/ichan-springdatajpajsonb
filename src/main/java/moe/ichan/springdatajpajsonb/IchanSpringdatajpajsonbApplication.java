b2a8fff12b6c4ae60229f8dc102cf7a00b5a3b65
ichan-akira <18476854+ichan-akira@users.noreply.github.com>
2024-09-14 02:50:37 +0700

    feat: initial commit from spring initializr

---
+++ src/main/java/moe/ichan/springdatajpajsonb/IchanSpringdatajpajsonbApplication.java
@@ -0,0 +1,13 @@
+package moe.ichan.springdatajpajsonb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IchanSpringdatajpajsonbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IchanSpringdatajpajsonbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moe/ichan/springdatajpajsonb/IchanSpringdatajpajsonbApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.ichan.springdatajpajsonb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IchanSpringdatajpajsonbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
